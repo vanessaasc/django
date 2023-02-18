decb13cb0e1a2a33e7fcbd33bb4ff72d31172d56
unknown <SENA@IBAAPRCLLFSP079.FormacionSena.RED>
2023-02-18 11:45:37 -0500

    primer commit

---
+++ tareas/urls.py
@@ -0,0 +1,27 @@
+"""tareas URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from todo import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path ("", include("todo.urls")),
+    path ("",views.home,name ="home"),
+    path("eliminar/<int:tarea_id>/", views.eliminar,name="eliminar"),
+    path ("editar/<int:tarea_id>/", views.editar,name="editar"),
+]
